6290ffe5a7aa218d1659f2e22aee149f1e353d3f
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-10-07 17:32:11 +0500

    Create String.py

---
+++ String.py
@@ -0,0 +1,27 @@
+#String(Matn bilan ishlash)
+
+# ism = "Eshmat"
+# familiya = "Toshmatov"
+# print(ism,familiya)
+
+# ism = "Eshmat"
+# familiya = "Toshmatov"
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif)
+
+
+# ism = "Eshmat"
+# familiya = "Toshmatov"
+# print(f"Salom siz {ism} {familiya}misiz")
+
+# print("Hello world")
+# print("Hello\tworld") #---- orada bo'shli ochadi
+# print("Hello\nworld") #----abzas ochadi
+
+# ism = "Eshmat"
+# familiya = "Toshmatov"
+# ism_sharif = f"{ism} {familiya}"
+# print(ism_sharif.upper())  # --- HAMMA HARFLAR KATTA
+# print(ism_sharif.lower()) #---- hamma harf kichkina
+# print(ism_sharif.title()) #---- Birinchi Harf Katta
+# print(ism_sharif.capitalize())  #---Faqat so'z boshi katta harf  

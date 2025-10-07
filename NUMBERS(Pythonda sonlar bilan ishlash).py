7b2b9597df17b8484fcc672f435b0f7847284cbd
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-10-07 18:19:16 +0500

    Create NUMBERS(Pythonda sonlar bilan ishlash).py

---
+++ NUMBERS(Pythonda sonlar bilan ishlash).py
@@ -0,0 +1,68 @@
+# NUMBERS(Pythonda sonlar bilan ishlash)
+
+# a = 20  # Sonlar musbat yoko
+# b = -30 # manfiy bo'lishi mumkin
+# c = a + b
+# print(c)
+
+# Kvadratning yuzini hisoblaymiz
+# kvdrt_tmni = 20 # Kavdratning tomoni 20 ga teng
+# kvdrt_yuzi = kvdrt_tmni**2 # Kvadrat yuzini hisoblaymiz
+# print(kvdrt_yuzi)
+
+# pi = 3.14159 # o'nlik son (float)
+# radius = 10  # butun son (integer)
+# diametr = 2*radius
+# print("Aylana uzunligi ", pi*diametr, " ga teng.")
+
+# a = -20
+# b = 40
+# c = b/a
+# print(c) # natija o'nlik son bo'ladi
+
+# a = 2
+# b = 3.0
+# # Quyidagi arifmetik amallarning natijasi o'nlik son hosil qiladi
+# print(a+b) 
+# print(a*b)
+# print(a**b)
+# print(2*(a+b))ism = 'Jobir'
+
+# ism = "Ali"
+# yosh = 36
+# xabar = ism + ' ' + str(yosh) + ' yoshda'
+# print(xabar)
+
+# ism = 'Ali'
+# yosh = 38
+# print(type(ism))  # ism degan o'zgaruvchining turini konsolga chiqaramiz
+# print(type(yosh)) # ismyosh degan o'zgaruvchining turini konsolga chiqaramiz
+
+# foydalanuvchining tug'ilgan yilini so'raymiz
+# t_yil =input("Tug'ilgan yilingizni kiriting: ")
+# #2 foydalanuvchi yoshini xisoblaymiz
+# yosh = 2025 - int(t_yil) # 
+# #3 foydalanuvchi yoshini konsolga chiqaramiz
+# print("Siz ",yosh ,"yoshda ekansiz")
+
+# son = int(input("Istalgan soningizni kiriting:"))
+# son_kv = son**2
+# son_kub = son**3
+# print(f"Siz kiritgan son {son},uning kvadrati {son_kv} ga,kubi {son_kub} ga teng")
+
+# son_1 = int(input("Birinchi sonni kiriting:"))
+# son_2 = int(input("Ikkinchi sonni kiriting:"))
+# print(f"{son_1} + {son_2} = {son_1 +son_2}")
+# print(f"{son_1} - {son_2} = {son_1 -son_2}")
+# print(f"{son_1} * {son_2} = {son_1 *son_2}")
+# print(f"{son_1} / {son_2} = {son_1 /son_2}")
+
+# a = int(input("Birinchi sonni kiriting:"))
+# b = int(input("Ikkinchi sonni kiriting:"))
+# print(f"{a} + {b} =",a +b)
+# print(f"{a} - {b} =",a -b)
+# print(f"{a} * {b} =",a *b)
+# print(f"{a} / {b} =",a /b)
+
+
+

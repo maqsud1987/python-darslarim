a1ac3579f154f56f2f2b2c9b3a88a4ab041a8ee6
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-10-07 15:31:56 +0500

    Create O'zgaruvchilar.py

---
+++ O'zgaruvchilar.py
@@ -0,0 +1,20 @@
+# O'zgaruvchi - kompyuter xotirasida ma'lum bir qiymatni saqlash uchun ajratilgan joy.
+
+# ism = "Abdulloh"
+# yosh = 25
+# print(ism)
+# print(yosh)
+
+
+# ism = "Abdulloh"
+# print(ism)
+# ism="Muhammad"
+# print(ism)
+
+# radius = 5
+# PI= 3.14
+# diametr = 10
+# yuz = PI*(radius)**2
+# uzunlik = PI*diametr
+# print("Doira yuzi S =",yuz,"Aylana uzunligi L =",uzunlik,"ga teng")
+

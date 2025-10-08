2d0c9e0831c7e727214f2661ee7adc0028af9fe9
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-10-08 11:49:45 +0500

    Create LISTS(RO'YXATLAR).py

---
+++ LISTS(RO'YXATLAR).py
@@ -0,0 +1,40 @@
+#LISTS(RO'YXATLAR BILAN ISHLASH)
+
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# cars.sort()
+# print(cars)
+
+# cars = ['bmw','mercedes benz', 'volvo', 'general motors', 'tesla', 'audi']
+# cars.sort(reverse=True)  # --- ro'yxatni tartib bilan aylantirish
+# print(cars)
+
+# mehmonlar = ['Odil', 'Hamid', 'Temur', 'Avazbek', 'Farruh', 'Shamsiddin']
+# print( sorted(mehmonlar))
+
+# ages = [12, 98, 34, 65, 34, 76, 11]
+# ages.sort()
+# print(ages)
+# print(sorted(ages, reverse=True))
+
+# fruits = ['pear','banana','lemon','apple','waterlemon']
+# fruits.reverse()
+# print(fruits) # --- ro'yxatni aylantirish
+
+# fruits = ['pear','banana','lemon','apple','waterlemon']
+# print(f"Elementlar soni {len(fruits)} ta")
+
+# j_sonlar = list(range(0,41,2))
+# print(j_sonlar)
+# j_sonlar.sort(reverse=True)
+# print(j_sonlar)
+
+ismlar = []
+ismlar.append("Ali")
+ismlar.append("Vali")
+ismlar.append("Sherzod")
+ismlar.append("Salim")
+savol = input((f"{ismlar[3]} bugun choyxona bormi >>>").title())
+print(f"ASalom {ismlar[0]} bugun choyxonaga  borasanmi ?")
+print(savol)
+
+

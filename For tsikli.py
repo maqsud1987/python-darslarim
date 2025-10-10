b524af43684fec473eef0a95297235e8cadf5f66
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-10-10 15:46:29 +0500

    for

---
+++ For tsikli.py
@@ -0,0 +1,158 @@
+# # For yordamida kodni takrorlash
+# mehmonlar = ["Ali","Vali","Salom","Olim","Karim"]
+# for mehmon in mehmonlar:
+#     print("Salom",mehmon)
+
+# mehmonlar = ["Ali","Vali","Salom","Olim","Karim"]
+# for mehmon in mehmonlar:
+#     print(f"Salom {mehmon},sizni 20- oktabr kungi nahor oshga taklif qilamiz")
+#     print("Hurmat bilan Eshmatovlar oilasi")
+
+# sonlar=list(range(1,10))
+# for son in sonlar:
+#     print(f"{son} ning kvadrati {son} ga teng")
+
+# sonlar=list(range(1,21))
+# sonlar_kvadrati = []
+# for son in sonlar:
+#     sonlar_kvadrati.append(son**2)
+# print(sonlar)
+# print(sonlar_kvadrati)
+
+# print("Beshta eng yaqin do'stingizni kiriting")
+# dostlar = []
+# for n in range(5):
+#     dostlar.append(f"{n+1} do'stingizni kiriting:")
+# print(dostlar)
+
+# print("Uchta  formula kiriting :")
+# formulalar=[]
+# for n in range(3):
+#     formulalar.append(input(f"{n+1} - formula>>> "))
+# print(formulalar)
+
+# ismlar = ["Anvar","Ali","Salim","Zohid","Hikmat"]
+# for ism in ismlar:
+#     print(f"Salom {ism}")
+# print(f"Kod {len(ismlar)} marta takrorlandi")
+
+# toq_sonlar = list(range(11,101,2))
+# for toq_son in toq_sonlar:
+#     print(toq_son**3)
+
+# print("Sevimli 5 ta kinongizni kiriting: ")
+# kinolar = []
+# for n in range(5):
+#         kinolar.append(input(f"{n+1} - kinoni kiriting >>>"))
+# print(f"Siz sevgan kinolar - {kinolar}")
+
+# odamlar =int(input("Bugun necha kishi bilan uchrashdingiz:"))
+# ismlar = []
+# for n in range(odamlar):
+#     ismlar.append(input(f"{n+1} - odamingiz kim edi: "))
+# #print(odamlar)
+# print(f"Demak siz {len(ismlar)} ta odam bilan uchrashgansiz,ular {ismlar}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

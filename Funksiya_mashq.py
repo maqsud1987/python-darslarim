f96edec6510807626a5144282d3bdc2e681a0c19
maqsud1987 <maqsudsulaymonov86@gmail.com>
2025-12-23 21:59:57 +0500

    Create Funksiya_mashq.py

---
+++ Funksiya_mashq.py
@@ -0,0 +1,77 @@
+# def ism_yosh_top(ism,yil):
+#     """Foydalanuvchi ismi va yoshini so'rab, 
+#     uning tug'ilgan yilini hisoblaydigan funksiya """
+#     yosh = 2025 - yil
+#     print(f"Foydalanuvchining isi {ism.title()},{yosh} yoshda")
+    
+# ism_yosh_top('mustafo', 1986)
+
+
+# def son_kv_top(son):
+#     """Foydalanuvchidan son olib, uning kvadrati 
+#     va kubini konsolga chiqaruvchi funksiya"""
+#     print(f"{son} ning kvadrati {son**2} ga,kubi esa {son**3} ga teng")
+
+# son_kv_top(5)
+
+
+# def ikki_barobar(son):
+#     """Berilgan sonning 2 barobarini qaytaradi"""
+#     return son * 2
+
+
+# son = int(input("Sonni kiriting: "))
+# natija = ikki_barobar(son)
+# print("Natija:", natija)
+
+
+
+
+
+
+
+def uch_barobar(son):
+    """Foydalanuvchidan bitta son qabul qilib
+    shu sonning 3 barobarini hisoblovchi funksiya"""
+    return son*3
+
+son  = int(input("Sonni kiriting : "))
+natija = uch_barobar(son)
+print(f"{son} ning 3 barobari,{natija}")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

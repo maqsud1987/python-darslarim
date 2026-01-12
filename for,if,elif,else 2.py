5fc68e884ba259458d3f5dbb8979522799749573
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-01-12 21:53:02 +0500

    .

---
+++ for,if,elif,else 2.py
@@ -0,0 +1,103 @@
+#----Foydalanuvchidan istalgan son kiritishni so'rang. 
+#----Agar son manfiy bo'lsa konsolga "Manfiy son", 
+#----agar musbat bo'lsa "Musbat son" degan xabarni chiqaring. ---#
+
+# son  = int(input("Sonni kiriting: "))
+
+#--- variant_1 NATIJA OLISH -----
+# if son<0:
+#     print("Son manfiy")
+# else:
+#     print("Son musbat")
+    
+#--- variant_2 NATIJA OLISH -----
+#print("Son manfiy") if son<0 else print("Son musbat")
+
+
+
+
+# #sonning ildizini chiqaruvchi dastur
+# son  = float(input("Sonni kiriting: "))
+# print(son**(0.5)) if son > 0 else print("Bu son manfiy")
+
+
+    
+#Foydalanuvchidan juft son kiritishni so'rang. Agar foydalanuvchi 
+#juft son kiritsa "Rahmat!", agar toq son kiritsa "Bu son juft emas" 
+#degan xabarni chiqaring.
+
+# son = int(input("Juft sonni kiriting : "))
+# print("Bu juft son") if son%2 ==0  else  print("Bu toq son")
+
+
+
+#Foydalanuvchi yoshini so'rab,muzeyga kirish uchun chipta narhini hisoblovchi dastur
+
+# yosh = int(input("Yoshingizni kiriting: "))
+# if yosh <=0 or yosh>100:
+#     print("Siz yoshingizni xato kiritdingiz,iltimos qayta kiriting")
+# else:
+#     if 0<yosh<=4 or yosh>60:
+#         narh = 0
+
+#     elif 4<yosh<18:
+#         narh = 10000
+        
+#     elif 18<=yosh<60:
+#         narh = 20000
+        
+#     print(f"Sizga kirish {narh} so'm")
+
+
+
+#-------Foydalanuvchidan ikita son kiritishni so'rang, 
+# sonlarni solishtiring va ularning teng yoki katta/kichikligi 
+# haqida xabarni chiqaring ---------------------------#
+
+# x = float(input("Birinchi sonni kiriting :"))
+# y = float(input("Ikkinchi sonni kiriting :"))
+
+# if x>y:
+#     print(f"{x}>{y}")
+# elif x<y:
+#     print(f"{x}<{y}")
+# else:
+#     print(f"{x}={y}")
+
+
+
+mahsulotlar = ['ruchka','kitob','olma','nok','qalam','non','piyoz','choy','un','suv']
+savat = []
+print("5 ta mahsulotmi kiriting: ")
+for n in range(5):
+    savat.append(input(f"{n+1}-mahsulotni kiriting : "))
+    for mahsulot in savat:
+        if mahsulot in mahsulotlar:
+            print("Mahsulot do'konimizda bor")
+        else:
+            print("Mahsulot do'konimizda yo'q")  # kami bor ertaga tuzatamiz
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- for,if.py
+++ for,if.py
@@ -1,172 +1,24 @@
-#1---Berilgan ro'yxatdan juft sonni topuvchi dastur-----
-# sonlar = [4, 31, 13, 11, 7] 
-# juft_sonlar = []
-# for son in sonlar:   
-#     if son %2 ==0:
-        
-#         juft_sonlar.append(son/2)
-        
-# if len(juft_sonlar)==0:   20
-#     print("Bu ro'yxatda juft son yo'q")
+#----Foydalanuvchidan istalgan son kiritishni so'rang. 
+#----Agar son manfiy bo'lsa konsolga "Manfiy son", 
+#----agar musbat bo'lsa "Musbat son" degan xabarni chiqaring. ---#
+
+son  = int(input("Sonni kiriting: "))
+
+#--- variant_1 NATIJA OLISH -----
+# if son<0:
+#     print("Son manfiy")
 # else:
-#     print(juft_sonlar)
-
-
-#2------Foydalanuvchi kiritgan sonning kvadrati va kubini 
-#   konsolga chiqaruvchi dastur -------------
-# son = int(input("Sonni kiriting : "))
-# a = son**2
-# b = son**3
-# print(f"Soz kiritgan son {son} uning kvadrati {a},kubi esa {b} ga teng")
-
-
-
-#3-------Foydalanuvchining yoshini so'rab, uning tug'ilgan yilini hisoblab, 
-#  konsolga chiqaruvchi dastur -------------------------
-
-# t_yil = int(input("Tug'ilgan yilingizni kiriting : "))
-# yosh = 2026 - t_yil
-# print(f"Siz {yosh} yoshdasiz")
-
-
-#4-----Foydalanuvchidan ikki son kiritshni so'rab, kiritilgan sonlarning 
-#yig'indisi, ayirmasi, ko'paytmasi va bo'linmasini chiqaruvchi dastur---------
-
-# x=int(input("Birinchi sonni kiriting : >>>"))
-# y=int(input("Ikkinchi sonni kiriting : >>>"))
-# if y == 0:
-#     print("0 ga bolish mumkinmas")
-# else:
-#     print(f"{x}-{y}={x-y}")
-#     print(f"{x}+{y}={x+y}")
-#     print(f"{x}*{y}={x*y}")
-#     print(f"{x}/{y}={x/y}")
-
-
-#-------------- TAKRORLASH --------------
-#1
-# sonlar = [13,21,13,2221,111,3,131,11]
-# juft_sonlar=[]
-# for son in sonlar:
-#     if son%2==0:
-#         juft_sonlar.append(son*2)
-        
-# if len(juft_sonlar)==0:
-#    print("Bu ro'yxatda juft son yo'q")
-# else:
-#     print(f"Bu ro'yxatda {juft_sonlar} juf ")
-   
-#2
-# son = int(input("Sonni kiriting: >>>"))
-# print(f"Siz kiritgan son {son},uning kvadrati {son**2},kubi esa {son**3} ga teng ")
-
-        
-#3
-# yosh = int(input("Yoshingizni kiriting : "))
-# t_yil = 2026 - yosh
-# print(f"Siz {t_yil}-yilda tug'ilgansiz")   
-
-
-#4
-# son_1 = int(input("Birinchi sonni kiriting: "))        
-# son_2 = int(input("Ikkinchi sonni kiriting: "))
-# if son_2 ==0:
-#         print(" 0 ga bo'lish mumkin emas ")
-# else:
-#         print(f"{son_1} + {son_2} = {son_1+son_2}")
-#         print(f"{son_1} - {son_2} = {son_1-son_2}")
-#         print(f"{son_1} . {son_2} = {son_1*son_2}")
-#         print(f"{son_1} : {son_2} = {son_1/son_2}")
-
-# davlatlar = ['aqsh','turkmaniston','rossiya','angliya','xitoy']
-
-# son = list(range(120,1200,2))
-# print(son[0:10])
-
-# taomlar = ['osh','manti','somsa','shurchoy','dimlama']
-# nonushta = taomlar[:]
-
-# del nonushta[:3]
-# del nonushta [-1]
-# nonushta.append('sut')
-# nonushta.append('sir')
-# print(taomlar)
-# print(nonushta)
-
-
-# ismlar = ['ali','vali','salim','karim','olim']
-# for ism in ismlar:
-#     print(f"Salom {ism.title()} qlaysan")
+#     print("Son musbat")
     
-#     print(f"Kod {len(ismlar)} marta takrorlandi")
+#--- variant_2 NATIJA OLISH -----
+print("Son manfiy") if son<0 else print("Son musbat")
 
 
+#sonning ildizini chiqaruvchi dastur
+# son  = int(input("Sonni kiriting: "))
 
-# toq_sonlar = list(range(11,100,2,))
-# for son in toq_sonlar:
-#     print(f"{son}**3= {son**3}")
+# print(son**(0.5)) if son > 0 else print("Bu son manfiy")
 
-
-# s_kinolar = []
-# for n in range(5):
-#     s_kinolar.append(input(f"{n+1} - kinongizni kiriting: "))
-       
-# print(s_kinolar)
-
-
-
-# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-# for car in cars:
-#     if car == 'gm':
-#         print(car.upper())
-#     else:
-#         print(car.title())
-
-
-# ism = input("Ismingizni kiriting : ")
-# if ism.lower() =='admin':
-#     print("Xush kelibsiz, Admin. Foydalanuvchilar ro'yxatini ko'rasizmi?")
-# else:
-#     print(f"Xush kelibsiz {ism.title()}")
-
-
-
-# x = int(input("Birinchi sonni kiriting: "))
-# y = int(input("Ikkinchi sonni kiriting: "))
-# if x>y:
-#     print("Birinchi son katta")
     
-# elif x<y:
-        
-#     print("Ikkinchi son katta")
-        
-# else:
-#     print("Ikkala son teng")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    

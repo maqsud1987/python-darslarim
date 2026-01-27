a4bea1e2088f5d4486062d65da67f1f92d519266
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-01-27 22:48:51 +0500

    Create Funksiya_mashq_6.py

---
+++ Funksiya_mashq_6.py
@@ -0,0 +1,68 @@
+# ================= AMALIYOT ==========================
+
+# def mijoz_info(ism, familiya, tyil, tjoy, email='',tel=None):
+#     """Mijoz haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#     mijoz = {'ism':ism,
+#              'familiya':familiya,
+#              'tyil':tyil,
+#              'yoshi':2020-tyil,
+#              'tjoy':tjoy,
+#              'email':email,
+#              'telefon':tel}
+#     return mijoz
+# n = mijoz_info('Ali', 'Raupov', 1986, 'Buxoro')
+# print(n)
+
+
+# ======== UCHTA SONDAN KATTASINI TOPUVCHI FUNKSIYA ===========
+
+# def taqqosla(x,y,z):
+#     katta = x
+#     if y>katta:
+#         katta = y
+#     if z>katta:
+#         katta =z
+    
+        
+#     return katta
+
+# print(taqqosla(3, -1, -100))
+# print(taqqosla(3, 4, 5))
+# print(taqqosla(5, 0, 25))
+
+
+# ========== AYLANA UZUNLIGINI ANIQLOVCHI FUNKSIYA ==========
+
+def aylana_info(radius,pi = 3.14):
+    aylana = {
+        'radius':radius,
+        'diametr':radius*2,
+        'perametr':2*pi*radius,
+        'yuzi': (pi*radius)**2
+        }
+    return aylana
+print(aylana_info(5))
+    
+    
+    
+
+   
+        
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+  
+
+
+    

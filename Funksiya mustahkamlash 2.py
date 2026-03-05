d82d24bbaa8bbe985436229183b6a768031f44c5
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-03-05 08:47:37 +0500

    Create Funksiya mustahkamlash 2.py

---
+++ Funksiya mustahkamlash 2.py
@@ -0,0 +1,50 @@
+# ======== Son 10–50 oralig‘idami tekshiruvchi funksiya yozing ========
+
+# def tekshir(son):
+#     if 10<son<50:
+#         return True
+#     else:
+#         return False
+# print(tekshir(42))
+# print(tekshir(51))
+# print(tekshir(10))
+
+# ======= Berilgan oraliqdagi sonlar yig'indisini aniqlovchi funksiya =======
+
+# def yigindi(a,b):
+#     return sum(range(a,b+1))
+# print(yigindi(10,99))
+
+# ======= 3 ta son ichidan o‘rtacha qiymatni qaytaring =========
+
+# def ortacha(a,b,c):
+#     return (a+b+c)//3
+# print(ortacha(3, 4, 5))
+
+# def ortacha(*sonlar):
+#     return sum(sonlar) / len(sonlar)
+# print(ortacha(3, 4, 5,10))
+        
+# # ====== Haroratga qarab “Sovuq”, “Iliq”, “Issiq” qaytaring ======
+
+# def ob_havo(harorat):
+#     if 5>=harorat:
+#         return "Havo sovuq"
+#     elif 6<=harorat<=20:
+#         return "Havo iliq"
+#     elif 21<=harorat:
+#         return "Havo issiq"
+        
+# print(ob_havo(42))
+# print(ob_havo(100))
+# print(ob_havo(5))
+# print(ob_havo(15))    
+# print(ob_havo(-5))
+
+# ========= Login va parol tekshiruvchi funksiya yozing ===========
+
+def maxfiylik(parol,login):
+    if parol=="salom":
+        return "Login to'g'ri"
+    if login = "alik":
+

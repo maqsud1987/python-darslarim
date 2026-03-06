cd1fe3444e89bc8d50dae4b8267f3f67d496b1cd
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-03-06 14:17:49 +0500

    Create Funksiya mustahkamlash 3.py

---
+++ Funksiya mustahkamlash 3.py
@@ -0,0 +1,62 @@
+# ========= Login va parol tekshiruvchi funksiya yozing ===========
+
+# def maxfiylik(parol,login):
+    
+#     if parol=="salom" and  login == "alik":
+#         return "Parol va login to'g'ri"
+#     else:
+#         return "Parol yoki login xato"
+
+# print(maxfiylik("ohlkj", "login"))
+# print(maxfiylik("salom", "alik"))
+
+# =============== Sonning oxirgi raqamini qaytaring ==================
+
+# def oxirgi_son(son) :
+    
+#     return abs(son)%10
+
+# print(oxirgi_son(12336))
+# print(oxirgi_son(123))
+# print(oxirgi_son(-208))
+
+# ============= Sonning birinchi raqamini qaytaring ===================
+
+# def birinchi_raqam(son):
+#     son = abs(son)      # manfiy bo‘lsa musbat qilamiz
+    
+#     while son >= 10:    # son 1 xonali bo‘lguncha
+#         son //= 10      # oxirgi raqamni olib tashlaymiz
+        
+#     return son
+
+# print(birinchi_raqam(4567))
+# print(birinchi_raqam(123))
+# print(birinchi_raqam(-98))
+# print(birinchi_raqam(7))
+
+# ============== Son 100 dan katta bo‘lsa 2 ga ko‘paytirib qaytaring, 
+# aks holda o‘zini qaytaring.========================================
+
+# def kopytirish(son):
+#     if son<100:
+#         return son*2
+#     else:
+#         return son
+    
+# print(kopytirish(20))
+# print(kopytirish(101)) 
+# print(kopytirish(100)) 
+
+# ========== Matn uzunligini qaytaruvchi funksiya yozing =========
+
+# def matn_uzunlik(matn):
+#     return len(matn)
+# print(matn_uzunlik("aftamatlashtirilganligidandir"))
+
+#===Matn katta harf bilan boshlanadimi tekshiruvchi funksiya yozing===
+
+def def katta_harf_bormi(matn):
+    
+        
+        

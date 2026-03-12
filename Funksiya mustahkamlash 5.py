3b37a21ef6cf04ed8559edd11c47f456bba76588
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-03-12 08:48:14 +0500

    Create Funksiya mustahkamlash 5.py

---
+++ Funksiya mustahkamlash 5.py
@@ -0,0 +1,107 @@
+# ============ Son tub ekanini tekshiruvchi funksiya yozing ==============
+# def tub_son_aniqla(son):
+    
+#     for x in range(2,son):
+#         if son%x==0:
+#             return "Tub emas"
+        
+#     return "Tub"
+        
+# print(tub_son_aniqla(2))        
+# print(tub_son_aniqla(521))
+# print(tub_son_aniqla(8))
+# print(tub_son_aniqla(3))
+# print(tub_son_aniqla(13))  
+
+# ============= List yig‘indisini qaytaruvchi funksiya yozing ==============
+# royxat = [2,3,4,5]
+
+# def yigindini_top(royxat):
+#     yigindi=0
+#     for n in royxat:
+#         yigindi +=n
+#     return yigindi
+
+# natija = yigindini_top(royxat)
+# print(natija)
+
+# ================ Listdagi eng katta sonni qaytaring ====================
+# royxat = [1,-100,0,0.2]
+# def kattasini_top(royxat):
+#     eng_katta = royxat[0]
+#     for n in royxat:
+#         if n>eng_katta:
+#             eng_katta=n
+#     return eng_katta
+# natija = kattasini_top(royxat)
+# print(natija)
+
+# ================ Listdagi eng kichik sonni qaytaring ===================
+# royxat = [1,-100,0,0.2]
+
+# def eng_kichik_top(royxat):
+#     eng_kichik=royxat[0]
+#     for n in royxat:
+#         if n<eng_kichik:
+#             eng_kichik=n
+#     return eng_kichik
+
+# natija=eng_kichik_top(royxat)
+# print(natija)
+
+# ================== Listdagi juft sonlarni qaytaring ====================
+# royxat = [1,3,5,10,4,6,11]
+# def juftni_top(royxat):
+#     juft_sonlar = []
+#     for n in royxat:
+#         if n%2==0:
+#             juft_sonlar.append(n)
+#     return juft_sonlar
+
+# natija = juftni_top(royxat)
+# print(natija)
+
+# ================= Listdagi toq sonlarni qaytaring =====================
+# royxat = [1,3,5,10,4,6,11]
+# def toq_son_aniqla(royxat):
+#     toq_son = []
+#     for n in royxat:
+#         if n%2 !=0:
+#             toq_son.append(n)
+#     return toq_son
+# natija = toq_son_aniqla(royxat)
+# print(natija)
+
+# ============== Listdagi manfiy sonlar sonini qaytaring ================
+# royxat = [1,-3,5,10,-4,6,11,-5,0]
+# def manfiy_son_aniqla(royxat):
+#     manfiy_son = []
+#     for n in royxat:
+#         if n<0:
+#             manfiy_son.append(n)
+#     return manfiy_son
+# natija = manfiy_son_aniqla(royxat)
+# print(natija)    
+    
+# ================ Listdagi o‘rtacha qiymatni qaytaring =================
+# royxat = [3,4,5]
+# def ortachasini_aniqla(royxat):    
+#    return sum(royxat)/len(royxat)
+# natija = ortachasini_aniqla(royxat)
+# print(natija)
+
+# ============= Listni teskari qaytaruvchi funksiya yozing ==============
+royxat = [3,4,5]
+ 
+
+
+    
+    
+
+
+    
+    
+
+    
+
+        

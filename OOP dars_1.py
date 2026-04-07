c786dc697a3d7e4f2ff86b915e173c3cae7e0df4
maqsud1987 <maqsudsulaymonov86@gmail.com>
2026-04-07 12:32:48 +0500

    Update OOP dars_1.py
    qo'shimcha qilinfi

--- OOP dars_1.py
+++ OOP dars_1.py
@@ -1,15 +1,42 @@
-class Talaba:
-    def __init__(self,ism,familiya,tyil):
-        self.ism = ism
-        self.familiya = familiya
-        self.t_yil = tyil
-    def yosh(self,joriy_yil):
-        return joriy_yil - self.t_yil
-    def tanishtir(self):
-        return f"Ismim {self.ism},familiyam {self.familiya},{self.t_yil} ta tug'ilganman"
-talaba1=Talaba("Salim","Olimov",1987)
-talaba2=Talaba("Olim","Salimov",2009)
-talaba3=Talaba("Halim","Karimov",1995)
+# ================== Foydalanuvchi uchun web sahifa ====================
+
+# class User:
+#     def __init__(self,ism,familiya,telefon,email = ""):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.tel = telefon
+#         self.email = email
+#     def get_info(self):
+#         return f"Foydalanuvchi:ismi {self.ism},familiyasi {self.familiya},telefon {self.tel} "
+        
+# foydalanuvchi1 = User("Ali","Salimov",77777)
+# print(foydalanuvchi1.ism)
+# print(foydalanuvchi1.get_info())
+
+# ==================== Kitoblar kutubxonasi ============================
+
+class book:
+    def __init__(self,kitob_nomi,muallif,nashr_yili,sahifa_soni):
+        self.kitob_nomi = kitob_nomi
+        self.muallif = muallif
+        self.nashr_yili = nashr_yili
+        self.sahifa_soni = sahifa_soni
+        
+    def get_info(self):
+        return (f"Kitob: {self.kitob_nomi},"
+                f"Muallif: {self.muallif},"
+                f" Yili: {self.nashr_yili},"
+                f"Sahifalari:{self.sahifa_soni}" )
+       
+        
+        
+book1 = book(f'Sherlok Xolms', "Arthur Conan Doyle", 1892, 307)
+book2 = book("O'tgan kunlar ", "Abdullo Qodiriy", 1926, 383)
+print(book1.get_info())
+print(book2.get_info())
+
+
+        
 
 
     
